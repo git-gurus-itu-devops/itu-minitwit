929230f2d8f47f9608854050de2b2ecf3011d28c
Frederik Petersen <fred.p@live.dk>
2024-02-02 13:30:30 +0100

    Change werkzeug submodule import

--- minitwit.py
+++ minitwit.py
@@ -17,7 +17,7 @@ from datetime import datetime
 from contextlib import closing
 from flask import Flask, request, session, url_for, redirect, \
      render_template, abort, g, flash
-from werkzeug import check_password_hash, generate_password_hash
+from werkzeug.security import check_password_hash, generate_password_hash
 
 
 # configuration
@@ -39,7 +39,8 @@ def init_db():
     """Creates the database tables."""
     with closing(connect_db()) as db:
         with app.open_resource('schema.sql') as f:
-            db.cursor().executescript(f.read())
+
+            db.cursor().executescript(f.read().decode('utf-8'))
         db.commit()
 
 

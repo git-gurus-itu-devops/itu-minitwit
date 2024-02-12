cd1d89f1ba1a3164f9e1d69253067aaa109d028f
Andreas Trøstrup <andreastrstrup@gmail.com>
2024-02-12 14:46:17 +0100

    Add ActiveRecord relations for messages and follow

--- models/message.rb
+++ models/message.rb
@@ -1,5 +1,6 @@
 class Message < ActiveRecord::Base
   self.table_name = "message"
+  belongs_to :author, class_name: "User"
 
   validates_presence_of :text
 end
--- models/user.rb
+++ models/user.rb
@@ -1,5 +1,21 @@
 class User < ActiveRecord::Base
   self.table_name = "user"
 
+  has_and_belongs_to_many :followers,
+    class_name: "User",
+    join_table: "follower",
+    foreign_key: "whom_id",
+    association_foreign_key: "who_id",
+    inverse_of: :following
+
+  has_and_belongs_to_many :following,
+    class_name: "User",
+    join_table: "follower",
+    foreign_key: "who_id",
+    association_foreign_key: "whom_id",
+    inverse_of: :followers
+
+  has_many :messages, foreign_key: :author_id
+
   has_secure_password
 end

0db8129d2a1421ce419847a62ee24e072f05fdf3
Andreas Trøstrup <andreastrstrup@gmail.com>
2024-02-13 16:08:43 +0100

    feat: hide flagged messages on timeline

--- models/message.rb
+++ models/message.rb
@@ -2,6 +2,8 @@ class Message < ActiveRecord::Base
   self.table_name = 'message'
   belongs_to :author, class_name: 'User'
 
+  scope :unflagged, -> { where(flagged: 0) }
+
   scope :authored_by, ->(users) { where(author: users) }
 
   validates_presence_of :text

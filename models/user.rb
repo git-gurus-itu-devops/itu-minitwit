61ec7665606f9395781a0e9922af0b7d435949fc
Andreas Trøstrup <andreastrstrup@gmail.com>
2024-03-22 13:21:56 +0100

    Autoformat

--- models/user.rb
+++ models/user.rb
@@ -1,19 +1,21 @@
-require 'digest'
+# frozen_string_literal: true
+
+require "digest"
 
 class User < ActiveRecord::Base
   has_and_belongs_to_many :followers,
-                          class_name: 'User',
-                          join_table: 'followers',
-                          foreign_key: 'whom_id',
-                          association_foreign_key: 'who_id',
-                          inverse_of: :following
+    class_name: "User",
+    join_table: "followers",
+    foreign_key: "whom_id",
+    association_foreign_key: "who_id",
+    inverse_of: :following
 
   has_and_belongs_to_many :following,
-                          class_name: 'User',
-                          join_table: 'followers',
-                          foreign_key: 'who_id',
-                          association_foreign_key: 'whom_id',
-                          inverse_of: :followers
+    class_name: "User",
+    join_table: "followers",
+    foreign_key: "who_id",
+    association_foreign_key: "whom_id",
+    inverse_of: :followers
 
   has_many :messages, foreign_key: :author_id
 
@@ -29,7 +31,7 @@ class User < ActiveRecord::Base
 
   def gravatar(size = 80)
     md5 = Digest::MD5.new
-    md5 << email.strip.downcase.encode('utf-8')
+    md5 << email.strip.downcase.encode("utf-8")
     md5_hash = md5.hexdigest
     "http://www.gravatar.com/avatar/#{md5_hash}?d=identicon&s=#{size}"
   end

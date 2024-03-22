21ad019edd6255d529ad130ba77ec10405c42143
Frederik Petersen <fred.p@live.dk>
2024-03-22 14:41:38 +0100

    Add await to expect in timeline test

--- e2e/timeline.ts
+++ e2e/timeline.ts
@@ -2,8 +2,12 @@ import { test, expect, Page } from "@playwright/test";
 
 test("Public timeline shows all messages", async ({ page }) => {
   await page.goto("/public");
-  expect(page.getByText("This is a message written by user 2")).toBeVisible();
-  expect(page.getByText("This is a message written by user 3")).toBeVisible();
+  await expect(
+    page.getByText("This is a message written by user 2")
+  ).toBeVisible();
+  await expect(
+    page.getByText("This is a message written by user 3")
+  ).toBeVisible();
 });
 
 test("My timeline only shows messages from followed users", async ({
@@ -13,6 +17,10 @@ test("My timeline only shows messages from followed users", async ({
   await page.fill("input[name=username]", "testuser");
   await page.fill("input[name=password]", "1234");
   await page.click("input[type=submit]");
-  expect(page.getByText("This is a message written by user 2")).toBeVisible();
-  expect(page.getByText("This is a message written by user 3")).toBeHidden();
+  await expect(
+    page.getByText("This is a message written by user 2")
+  ).toBeVisible();
+  await expect(
+    page.getByText("This is a message written by user 3")
+  ).toBeHidden();
 });

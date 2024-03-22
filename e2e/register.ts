b142395aaeca16cb4aa4c20267f3b0f2383c2c51
Frederik Petersen <fred.p@live.dk>
2024-03-22 13:35:50 +0100

    Register e2e test

---
+++ e2e/register.ts
@@ -0,0 +1,45 @@
+import { test, expect } from "@playwright/test";
+
+test.beforeEach(async ({ page }) => {
+  await page.goto("/register");
+});
+
+test("should succesfully register a new user", async ({ page }) => {
+  await page.fill("input[name=username]", "newuser");
+  await page.fill("input[name=email]", "new@user.dk");
+  await page.fill("input[name=password]", "1234");
+  await page.fill("input[name=password2]", "1234");
+  await page.click("input[type=submit]");
+  await expect(page).toHaveURL(/.*login/);
+});
+
+test("Should fail if username and email is taken", async ({ page }) => {
+  await page.fill("input[name=username]", "testuser");
+  await page.fill("input[name=email]", "test@user.dk");
+  await page.fill("input[name=password]", "1234");
+  await page.fill("input[name=password2]", "1234");
+  await page.click("input[type=submit]");
+  await expect(
+    page.getByText(
+      "Username has already been taken, Email has already been taken"
+    )
+  ).toBeVisible();
+});
+
+test("Should fail if email is taken", async ({ page }) => {
+  await page.fill("input[name=username]", "testuser2");
+  await page.fill("input[name=email]", "test@user.dk");
+  await page.fill("input[name=password]", "1234");
+  await page.fill("input[name=password2]", "1234");
+  await page.click("input[type=submit]");
+  await expect(page.getByText("Email has already been taken")).toBeVisible();
+});
+
+test("Should fail if username is taken", async ({ page }) => {
+  await page.fill("input[name=username]", "testuser");
+  await page.fill("input[name=email]", "test@user.dk2");
+  await page.fill("input[name=password]", "1234");
+  await page.fill("input[name=password2]", "1234");
+  await page.click("input[type=submit]");
+  await expect(page.getByText("Username has already been taken")).toBeVisible();
+});

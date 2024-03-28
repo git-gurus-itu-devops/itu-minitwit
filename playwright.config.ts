7945740678f9561a9f6e80464fa3d425fba320b3
Silke Bonnen <silke.bonnen@gmail.com>
2024-03-28 13:19:48 +0100

    adding seed

--- playwright.config.ts
+++ playwright.config.ts
@@ -29,7 +29,7 @@ export default defineConfig({
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
-    baseURL: "http://minitwit:5000" ,
+    baseURL: process.env.CI ? "http://minitwit:5000" : "http://127.0.0.1:4567",
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: "on-first-retry",

05640d822a4f8c63ca1df45e275e4e9d969c28ea
Silke Bonnen <silke.bonnen@gmail.com>
2024-03-31 14:39:05 +0200

    WIP

--- simapi/simulator_entrypoint.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Execute the first command
-python ./minitwit_simulator.py "http://minitwit-api:5001" true > ./simulator_output.txt
-
-# Execute the second command
-pytest ./minitwit_simulator_test.py

4081890d7ac85b478a3ecb450d6654ce7129aeb5
Frederik Petersen <fred.p@live.dk>
2024-04-02 13:46:21 +0200

    Remove very long timeouts

--- playwright.config.ts
+++ playwright.config.ts
@@ -16,16 +16,12 @@ export default defineConfig({
   fullyParallel: true,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
-  timeout: 5 * 60 * 1000, // 5 minutes
-  expect: {
-    timeout: 20 * 1000, // 20 seconds
-  },
   /* Retry on CI only */
   retries: process.env.CI ? 2 : 0,
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: "line",
+  reporter: process.env.CI ? "html" : "line",
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */

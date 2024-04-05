ba3d61ee72092711c03ce7c4de3d7cf8ac774c4a
Frederik Petersen <fred.p@live.dk>
2024-04-05 14:33:41 +0200

    Add registration with non matching passwords test

--- e2e/register.ts
+++ e2e/register.ts
@@ -43,3 +43,14 @@ test("Should fail if username is taken", async ({ page }) => {
   await page.click("input[type=submit]");
   await expect(page.getByText("Username has already been taken")).toBeVisible();
 });
+
+test("Should fail is passwords do not match", async ({ page }) => {
+  await page.fill("input[name=username]", "testuser2");
+  await page.fill("input[name=email]", "test@user.dk2");
+  await page.fill("input[name=password]", "1234");
+  await page.fill("input[name=password", "12345");
+  await page.click("input[type=submit]");
+  await expect(
+    page.getByText("Password confirmation doesn't match Password")
+  ).toBeVisible();
+});

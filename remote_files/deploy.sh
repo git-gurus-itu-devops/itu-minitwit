93bd42366960596e9e987056515c09de5105e41d
Mads Roager <94063304+MadsRoager@users.noreply.github.com>
2024-04-12 14:53:22 +0200

    Docker swarm (#64)
    * docker swarm
    
    * working wuhu
    
    * changing color
    
    * docker swarm run
    
    * ready
    
    * color
    
    * 3 replicas
    
    * whitespacce
    
    ---------
    
    Co-authored-by: Silke Bonnen <silke.bonnen@gmail.com>

--- remote_files/deploy.sh
+++ remote_files/deploy.sh
@@ -5,5 +5,4 @@ export DATABASE_URL=$1
 export NR_LICENSE_KEY=$2
 export MINITWIT_VERSION=$3
 
-docker compose -f docker-compose.yml pull
-docker compose -f docker-compose.yml up -d
+docker stack deploy -c docker-compose.yml minitwit

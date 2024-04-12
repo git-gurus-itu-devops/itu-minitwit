72cbc3bddcc38254eed4518f51185e63f50e70df
Frederik Petersen <fred.p@live.dk>
2024-04-12 09:50:55 +0200

    Fix type

--- e2e/register.ts
+++ e2e/register.ts
@@ -44,7 +44,7 @@ test("Should fail if username is taken", async ({ page }) => {
   await expect(page.getByText("Username has already been taken")).toBeVisible();
 });
 
-test("Should fail is passwords do not match", async ({ page }) => {
+test("Should fail if passwords do not match", async ({ page }) => {
   await page.fill("input[name=username]", "testuser2");
   await page.fill("input[name=email]", "test@user.dk2");
   await page.fill("input[name=password]", "1234");

397471b9cbecb85eb0ae4d657305a5bcf415211f
Andreas Trøstrup <andreastrstrup@gmail.com>
2024-04-30 22:52:02 +0200

    Add initial Terraform setup

--- .infrastructure/stack/deploy.sh
+++ .infrastructure/stack/deploy.sh
@@ -1,8 +1,6 @@
-cd /minitwit
-
 # Set by continous-deployment, pulled from GH secrets
 export DATABASE_URL=$1
 export NR_LICENSE_KEY=$2
 export MINITWIT_VERSION=$3
 
-docker stack deploy -c docker-compose.yml minitwit
+docker stack deploy -c minitwit_stack.yml minitwit
